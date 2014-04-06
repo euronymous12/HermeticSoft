9df73c8a1d2eeb06fdf69180fabacec8c7ba5e90
Carlos Morales <euronymous@outlook.com>
2014-04-06 15:45:34 -0600

    commit

--- HS_Aquiles_ERP/HS_Aquiles_ERP/App_Start/RouteConfig.cs
+++ HS_Aquiles_ERP/HS_Aquiles_ERP/App_Start/RouteConfig.cs
@@ -14,7 +14,7 @@ namespace HS_Aquiles_ERP {
             routes.MapRoute(
                 name: "Default", // Route name
                 url: "{controller}/{action}/{id}", // URL with parameters
-                defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional } // Parameter defaults
+                defaults: new { controller = "Accounts", action = "Login", id = UrlParameter.Optional } // Parameter defaults
             );
         }
 	}
---
+++ HS_Aquiles_ERP/HS_Aquiles_ERP/Web_Services/WS_Intranet.asmx
@@ -0,0 +1,1 @@
+﻿<%@ WebService Language="C#" CodeBehind="WS_Intranet.asmx.cs" Class="HS_Aquiles_ERP.Web_Services.WS_Intranet" %>
