d9ba031c7190f972eab053a5e2ea9378cfce2f1f
Carlos Morales <euronymous@outlook.com>
2014-04-06 10:49:38 -0600

    Creating Proyect

---
+++ HS_Aquiles_ERP/HS_Aquiles_ERP/App_Start/FilterConfig.cs
@@ -0,0 +1,10 @@
+using System.Web;
+using System.Web.Mvc;
+
+namespace HS_Aquiles_ERP {
+    public class FilterConfig {
+        public static void RegisterGlobalFilters(GlobalFilterCollection filters) {
+            filters.Add(new HandleErrorAttribute());
+        }
+    }
+}
